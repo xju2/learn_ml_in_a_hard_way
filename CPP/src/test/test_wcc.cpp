301a32ba1b04bc73f87a4c4d841bbb10ea61d032
Xiangyang Ju <xiangyang.ju@gmail.com>
2022-03-30 10:03:05 -0700

    wcc up

--- CPP/src/test/test_wcc.cpp
+++ CPP/src/test/test_wcc.cpp
@@ -11,20 +11,25 @@ int main()
     boost::vecS            // edge list
   , boost::vecS            // vertex list
   , boost::undirectedS     // directedness
-  , float                  // property associated with vertices
+  , boost::no_property     // property associated with vertices
+  , float                  // property associated with edges
   > Graph; 
 
     int v=6;
     Graph g(v);
 
-    boost::add_edge(0,1);
-    boost::add_edge(1,2);
-    boost::add_edge(2,3);
-    boost::add_edge(3,1);
-    boost::add_edge(4,5);
+    boost::add_edge(0, 1, 1.2, g);
+    boost::add_edge(1, 2, 0.2, g);
+    boost::add_edge(2, 3, 0.3, g);
+    boost::add_edge(3, 1, 3.4, g);
+    boost::add_edge(4, 5, 1.1, g);
 
     std::vector<int> component (boost::num_vertices (g));
-    size_t num_components = boost::connected_components (c, &component[0]);
+    size_t num_components = boost::connected_components (g, &component[0]);
     std::cout << "Number of components: " << num_components << std::endl;
+    // for (size_t i = 0; i < boost::num_vertices (c); ++i){
+    //   if (component[i] == 0)
+    //     std::cout << i << " ";
+    // }
     return 0;
 }
